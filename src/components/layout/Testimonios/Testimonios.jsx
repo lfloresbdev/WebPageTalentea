fd251aac2facf7121e0d384063a4a408650fa581
Leonardo Flores <U22223207@utp.edu.pe>
2026-07-21 13:00:55 -0500

    feat: Se configuro el colapso en los testimonios

--- src/components/layout/Testimonios/Testimonios.jsx
+++ src/components/layout/Testimonios/Testimonios.jsx
@@ -1,5 +1,5 @@
-import { FaUser } from "react-icons/fa";
 import { IoIosArrowDropright } from "react-icons/io";
+import ListaTestimonios from "./ListaTestimonios";
 
 function Testimonios() {
   return (
@@ -9,108 +9,10 @@ function Testimonios() {
                 <h2 className="text-3xl font-bold mb-8 text-sky-700 w-115"><a className="underline decoration-pink-500 decoration-3 underline-offset-15">Lo q</a>ue dicen nuestros <span className="text-pink-500">talentos</span></h2>
             
             </div>
-            
-            <div className="testimonios pt-10 mx-auto px-4 flex flex-wrap gap-9 items-center justify-center">
 
-                <div className="testimonio-1 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
+            <ListaTestimonios />
 
-                <div className="testimonio-2 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
-
-                <div className="testimonio-3 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
-
-                <div className="testimonio-4 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
-
-                <div className="testimonio-5 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
-
-                <div className="testimonio-6 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
-
-                <div className="testimonio-7 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
-
-                <div className="testimonio-8 w-128.5 h-50.25 flex gap-3">
-                    <div className="foto-testimonio">
-                        <FaUser className="size-30 text-sky-700"/>
-                    </div>
-                    
-                    <div className="contenido-testimonio">
-                        <p className="text-neutral-600">“Gracias a talentea encontré la oportunidad para crecer profesionalmente y seguir aprendiendo cada día”</p>
-                        <p className="text-sky-700 font-bold">Diego Lopez</p>
-                        <p className="text-neutral-600">Área de Marketing</p>
-                    </div>
-                </div>
-                
-            </div>
-
-            <div className="footer-testimonios mx-auto px-4 flex relative items-center justify-center gap-10 mt-10">
+            <div className="footer-testimonios mx-auto px-4 flex relative items-center justify-center gap-10">
                 <div className="bg-sky-700 w-176.5 h-59 rounded-lg p-6">
                     <h1 className="text-white font-bold text-3xl w-87 mb-3">¿Listos para su próximo desafío profesional?</h1>
                     <p className="text-white font-light text-sm w-74 mb-3">Postula ahora y da el siguiente paso a tu carrera o experiencia laboral</p>
